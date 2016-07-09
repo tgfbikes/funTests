ebc38d10b9bd7209dab49af137246eda4bf9446d
stvking <tgfbikes@gmail.com>
2016-07-09 17:52:33 -0600

    formating string with moment

--- app/service/milestoneService.js
+++ app/service/milestoneService.js
@@ -3,23 +3,23 @@ angular.module('int1')
   .factory('milestoneService', ['moment', function(moment) {
 
     var _milestones = [];
-	  
+
     var MileStone = function(name, date) {
       this.name = name;
-      this.date = new moment();
+      this.date = new moment(date).format('MMMM DD YYYY');
     };
 
     var milestoneService = {
-		
+
         createMilestone: function (name, date) {
 			var newMilestone = new MileStone(name, date);
 			_milestones.push(newMilestone);
         },
-		
+
 		getAllMilestones: function () {
 			return _milestones;
 		}
-		
+
     };
 
     return milestoneService;
