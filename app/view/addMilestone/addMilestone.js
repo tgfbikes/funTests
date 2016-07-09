9f334cf0a5e9c66c51d310ca3f30fc8682a2c712
stvking <tgfbikes@gmail.com>
2016-07-09 15:17:59 -0600

    trying about a thousand things to get the modal to close on save, WIP

--- app/view/addMilestone/addMilestone.js
+++ app/view/addMilestone/addMilestone.js
@@ -1,35 +1,46 @@
 angular.module('int1')
-    .factory("addMilestone", function ($rootScope, $uibModal) {
-		return {
-			open: function () {
-				var $modalInstance = $uibModal.open({
-					templateUrl: 'app/view/addMilestone/addMilestone.html',
-					controller: 'AddMilestoneCtrl as vm',
-					resolve: {	}
-				});
-				
-				var listener = $rootScope.$on('$stateChangeStart', function () {
-					$modalInstance.dismiss('State Changed');
-				});
-				
-				$modalInstance.result.finally(function () {
-					listener();
-				});
-				return $modalInstance;
-			}
-		};
-	}).controller('AddMilestoneCtrl', function ($scope, $uibModalInstance) {
-        var vm = this;
-        
-        vm.saveMilestone = saveMilestone;
-			
-		function saveMilestone () {
-			console.log('saveMilestone');
-        //     $uibModalInstance.close(result);
-        }
-        
-        var destroy = $scope.$on("$destroy", function () {
-            //Cleanup anything that would persist beyond destruction, including $on/$watch
-            destroy();
+
+  .factory("addMilestone", function ($rootScope, $uibModal) {
+
+    return {
+      open: function () {
+        var $modalInstance = $uibModal.open({
+          animation: true,
+          templateUrl: 'app/view/addMilestone/addMilestone.html',
+          controller: 'AddMilestoneCtrl as vm',
+          resolve: {}
         });
+
+       //  var listener = $rootScope.$on('$stateChangeStart', function () {
+			// console.log('firing');
+       //      $modalInstance.close('State Changed');
+       //  });
+      //
+       //  $modalInstance.result.then(function () {
+       //      listener();
+       //  });
+
+        return $modalInstance;
+        }
+      };
+	})
+
+  .controller('AddMilestoneCtrl', ['$scope', 'milestoneService', function ($scope, milestoneService, $uibModalInstance) {
+    var vm = this;
+
+    vm.name = '';
+    vm.date = '';
+    vm.saveMilestone = saveMilestone;
+
+    function saveMilestone () {
+      if (vm.name && vm.date) {
+          milestoneService.createMilestone(vm.name, vm.date);
+          // $uibModalInstance.close();
+      }
+    }
+
+    var destroy = $scope.$on("$destroy", function () {
+      //Cleanup anything that would persist beyond destruction, including $on/$watch
+      destroy();
     });
+  }]);
