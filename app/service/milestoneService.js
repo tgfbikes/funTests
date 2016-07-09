33a74a34f52b6376b6118a01cda0680d74ddc855
stvking <tgfbikes@gmail.com>
2016-07-09 15:17:17 -0600

    injecting moment, created private data storage, created Milestone function to make new milestones with, exporting service api

--- app/service/milestoneService.js
+++ app/service/milestoneService.js
@@ -1,6 +1,26 @@
-angular.module('int1').factory('milestoneService',function() {
+angular.module('int1')
 
-    var milestoneService = {};
-    
+  .factory('milestoneService', ['moment', function(moment) {
+
+    var _milestones = [];
+	  
+    var MileStone = function(name, date) {
+      this.name = name;
+      this.date = new moment();
+    };
+
+    var milestoneService = {
+		
+        createMilestone: function (name, date) {
+			var newMilestone = new MileStone(name, date);
+			_milestones.push(newMilestone);
+        },
+		
+		getAllMilestones: function () {
+			return _milestones;
+		}
+		
+    };
+
     return milestoneService;
-});
+}]);
