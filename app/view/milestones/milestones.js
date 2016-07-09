617600678c0b0581548e919068e8f79beab50817
stvking <tgfbikes@gmail.com>
2016-07-09 09:51:07 -0600

    gave vm.milestones a value to test ng-hide, working

--- app/view/milestones/milestones.js
+++ app/view/milestones/milestones.js
@@ -1,14 +1,15 @@
 angular.module('int1').config(function($stateProvider, $urlRouterProvider) {
-    
+
     $stateProvider.state('milestones', {
         url: '/milestones',
         controller: 'MilestonesCtrl as vm',
         templateUrl: 'app/view/milestones/milestones.html',
-    });    
+    });
 
 }).controller('MilestonesCtrl', function ($scope) {
- 
     var vm = this;
+	vm.milestones = ['hello'];
+	
     var destroy = $scope.$on("$destroy", function () {
         //Cleanup anything that would persist beyond destruction, including $on/$watch
         destroy();
