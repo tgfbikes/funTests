aeee21b842438138bf68a4f334a534394960ccc0
stvking <tgfbikes@gmail.com>
2016-07-09 10:16:31 -0600

    created mock data to test ng-repeat in view

--- app/view/milestones/milestones.js
+++ app/view/milestones/milestones.js
@@ -8,8 +8,17 @@ angular.module('int1').config(function($stateProvider, $urlRouterProvider) {
 
 }).controller('MilestonesCtrl', function ($scope) {
     var vm = this;
-	vm.milestones = ['hello'];
-	
+	vm.milestones = [
+		{
+			name: 'test1',
+			date: Date.now()
+		},
+		{
+			name: 'test',
+			date: Date.now()
+		}
+	];
+
     var destroy = $scope.$on("$destroy", function () {
         //Cleanup anything that would persist beyond destruction, including $on/$watch
         destroy();
