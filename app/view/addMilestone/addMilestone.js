f47eb672a28de8e13c1dd18868ba751bcfbeb891
stvking <tgfbikes@gmail.com>
2016-07-09 20:08:17 -0600

    wrapped in iife, strict mode and refactored to follow JP style guide

--- app/view/addMilestone/addMilestone.js
+++ app/view/addMilestone/addMilestone.js
@@ -1,60 +1,71 @@
-angular.module('int1')
+(function () {
+	'use strict';
+	
+	angular
+		.module('int1')
+		.factory("addMilestone", addMilestone)
+		.controller('AddMilestoneCtrl', AddMilestoneCtrl);
 
-  .factory("addMilestone", function ($rootScope, $uibModal) {
+	AddMilestoneCtrl.$inject = ['$scope', 'milestoneService'];
 
-    return {
-      open: function () {
-        var $modalInstance = $uibModal.open({
-          animation: true,
-          templateUrl: 'app/view/addMilestone/addMilestone.html',
-          controller: 'AddMilestoneCtrl as vm',
-          resolve: {}
-        });
+	function addMilestone ($rootScope, $uibModal) {
 
-        var listener = $rootScope.$on('$stateChangeStart', function () {
-			console.log('firing');
-            $modalInstance.close('State Changed');
-        });
-      
-        $modalInstance.result.then(function () {
-            listener();
-        });
+		return {
+			open: function () {
+				var $modalInstance = $uibModal.open({
+					animation: true,
+					templateUrl: 'app/view/addMilestone/addMilestone.html',
+					controller: 'AddMilestoneCtrl as vm',
+					resolve: {}
+				});
 
-        return $modalInstance;
-        }
-      };
-	})
+				var listener = $rootScope.$on('$stateChangeStart', function () {
+					console.log('firing');
+					$modalInstance.close('State Changed');
+				});
 
-  .controller('AddMilestoneCtrl', ['$scope', 'milestoneService', function ($scope, milestoneService, $uibModalInstance) {
-    var vm = this;
+				$modalInstance.result.then(function () {
+					listener();
+				});
 
-    vm.name = '';
-  	vm.date = '';
-  	vm.opened = false;
-    vm.saveMilestone = saveMilestone;
-    vm.openDatePopUp = openDatePopUp;
-    vm.dateOptions = {
-        formatYear: 'yy',
-        maxDate: new Date(2020, 5, 22),
-        minDate: new Date(),
-        startingDay: 1
-    };
-
-    function saveMilestone () {
-      if (vm.name && vm.date) {
-          milestoneService.createMilestone(vm.name, vm.date);
-		  vm.name = '';
-		  vm.date = '';
-          // $uibModalInstance.close();
-      }
-    }
-	  
-  	function openDatePopUp () {
-      vm.opened = true;
+				return $modalInstance;
+			}
+		};
 	}
 
-    var destroy = $scope.$on("$destroy", function () {
-      //Cleanup anything that would persist beyond destruction, including $on/$watch
-      destroy();
-    });
-  }]);
+	function AddMilestoneCtrl ($scope, milestoneService, $uibModalInstance) {
+		var vm = this;
+
+		vm.name = '';
+		vm.date = '';
+		vm.opened = false;
+		vm.saveMilestone = saveMilestone;
+		vm.openDatePopUp = openDatePopUp;
+		vm.dateOptions = {
+			formatYear: 'yy',
+			maxDate: new Date(2020, 5, 22),
+			minDate: new Date(),
+			startingDay: 1
+		};
+
+		function saveMilestone () {
+			if (vm.name && vm.date) {
+				milestoneService.createMilestone(vm.name, vm.date);
+				vm.name = '';
+				vm.date = '';
+				// $uibModalInstance.close();
+			}
+		}
+
+		function openDatePopUp () {
+			vm.opened = true;
+		}
+
+		var destroy = $scope.$on("$destroy", function () {
+			//Cleanup anything that would persist beyond destruction, including $on/$watch
+			destroy();
+		});
+	}
+	
+}());
+
