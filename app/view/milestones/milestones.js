29cbd7560c4008eca5bed153ac4fdfa015bb7d1d
stvking <tgfbikes@gmail.com>
2016-07-09 20:29:28 -0600

    wrapped in iife, use strict mode, and follwing JP style guide

--- app/view/milestones/milestones.js
+++ app/view/milestones/milestones.js
@@ -1,31 +1,45 @@
-angular.module('int1').config(function($stateProvider, $urlRouterProvider) {
+(function () {
+	'use strict';
+	
+	angular
+		.module('int1')
+		.config(config)
+		.controller('MilestonesCtrl', MilestonesCtrl);
 
-    $stateProvider.state('milestones', {
-        url: '/milestones',
-        controller: 'MilestonesCtrl as vm',
-        templateUrl: 'app/view/milestones/milestones.html',
-    });
+	MilestonesCtrl.$inject = ['$scope', 'addMilestone', 'milestoneService'];
 
-}).controller('MilestonesCtrl', ['$scope', 'addMilestone', 'milestoneService', function ($scope, addMilestone, milestoneService) {
-    var vm = this;
-	vm.orderBySelect = 'Date';
-	vm.orderBy = 'date';
-	vm.milestones = milestoneService.getAllMilestones();
-	vm.openModal = openModal;
-	vm.updateOrder = updateOrder;
+	function config($stateProvider, $urlRouterProvider) {
 
-	function openModal () {
-      addMilestone.open();
+		$stateProvider.state('milestones', {
+			url: '/milestones',
+			controller: 'MilestonesCtrl as vm',
+			templateUrl: 'app/view/milestones/milestones.html',
+		});
+
+	}
+
+	function MilestonesCtrl ($scope, addMilestone, milestoneService) {
+		var vm = this;
+		vm.orderBySelect = 'Date';
+		vm.orderBy = 'date';
+		vm.milestones = milestoneService.getAllMilestones();
+		vm.openModal = openModal;
+		vm.updateOrder = updateOrder;
+
+		function openModal () {
+			addMilestone.open();
+		}
+
+		function updateOrder (orderBy) {
+			vm.orderBy = orderBy;
+			orderBy === 'date' ? vm.orderBySelect = 'Date' : vm.orderBySelect = 'Name';
+		}
+
+		var destroy = $scope.$on("$destroy", function () {
+			//Cleanup anything that would persist beyond destruction, including $on/$watch
+			destroy();
+		});
 	}
 	
-	function updateOrder (orderBy) {
-		vm.orderBy = orderBy;
-		orderBy === 'date' ? vm.orderBySelect = 'Date' : vm.orderBySelect = 'Name';
-	}
+}());
 
-    var destroy = $scope.$on("$destroy", function () {
-      //Cleanup anything that would persist beyond destruction, including $on/$watch
-      destroy();
-    });
-
-}]);
